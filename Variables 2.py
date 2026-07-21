6edd0af8a9ade57b9fa6c459408ea5e2f1a973eb
Kolte Shravani Jaydeep <shravanikolte2103@gmail.com>
2026-07-21 14:32:18 +0530

    Add files via upload

---
+++ Variables 2.py
@@ -0,0 +1,11 @@
+state = "Maharashtra"
+capital = "Mumbai" 
+famous_cultural_place = "Pune"
+industrial_city = "Nagpur"
+information_technology_hub = "Mumbai"
+
+print(state)
+print(capital)
+print(famous_cultural_place)
+print(industrial_city)
+print(information_technology_hub)
